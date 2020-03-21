eb9ee446f59afe4352ac9ef7c388ee52d505c964
Abundiz <3duabundiz@gmail.com>
2020-03-21 11:07:59 -0600

    Componentes funcionales y props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import ReactDom from 'react-dom';
+ const TarjetaFruta = (props) =>{
+   console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>
+        {props.name}                
+      </h3>
+      
+      <p>${props.price}</p>
     </div>
   );
 }
+const App = () =>(
+  <div>
+    <TarjetaFruta name="sandia" price={2.00}/>
+    <TarjetaFruta name="Naranja" price={3.00} />
+    <TarjetaFruta name="Kiwi"price={5.00}/>
+    <TarjetaFruta name="Pina"price={2.50}>
+        
+    </TarjetaFruta>
+  </div>
+
+)
 
 export default App;

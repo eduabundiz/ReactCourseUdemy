6bed4932ba0f4a3bfb2a31d2672f297f89405b3b
Abundiz <3duabundiz@gmail.com>
2020-03-21 11:38:23 -0600

    metodos Manejadores eventos botones

--- src/App.js
+++ src/App.js
@@ -2,21 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReactDom from 'react-dom';
- const TarjetaFruta = (props) =>{
-   console.log(props)
-  return (
-    <div>
+
+class TarjetaFruta extends React.Component{
+  constructor(){
+    super()    
+    const METHODS =[
+      'agregar',
+      'quitar',
+      'limpiar'
+    ]
+    METHODS.forEach((method)=>{
+      this[method] = this[method].bind(this)
+    })
+    this.state = {
+      cantidad: 5
+    }
+  }
+  agregar(){    
+    this.setState({cantidad:this.state.cantidad+1})    
+  }
+  quitar(){    
+    this.setState({cantidad:this.state.cantidad-1})    
+  }
+  limpiar(){    
+    this.setState({cantidad:0})    
+  }
+  render(){
+    return(
+      <div>
       <h3>
-        {props.name}                
+        {this.props.name}                
       </h3>
-      
-      <p>${props.price}</p>
+    <div>Cantidad:{this.state.cantidad}</div>
+      <button onClick ={this.agregar}>+</button>
+      <button onClick ={this.quitar}>-</button>
+      <button onClick ={this.limpiar}>Limpiar</button>
+      <p>${this.props.price}</p>
     </div>
-  );
+    );
+  }  
 }
+
+
 const App = () =>(
   <div>
-    <TarjetaFruta name="sandia" price={2.00}/>
+    <TarjetaFruta name="sandia" price={8.00}/>
     <TarjetaFruta name="Naranja" price={3.00} />
     <TarjetaFruta name="Kiwi"price={5.00}/>
     <TarjetaFruta name="Pina"price={2.50}>
